3118b3dff1d327fe301f2f11022db6a5dfdd12dc
Lobinda <lobinda123@gmail.com>
2025-03-10 02:07:58 +0800

    again

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,11 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["https://ccore.newebpay.com", "http://localhost:3000"],
+    },
+  },
 };
 
 export default nextConfig;
